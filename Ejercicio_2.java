b932a5d735657b843f9fcc8ee4faadbfba9939e3
Josseluu <jslslpz@myuax.com>
2024-02-07 16:38:22 +0100

    .

--- Ejercicio_2.java
+++ Ejercicio_2.java
@@ -1,13 +1,13 @@
 
 public class Ejercicio_2 {
     public static void main(String[] args) {
-        // Mostrar el mayor número representable en char
+
         System.out.println("El mayor número representable en char es: " + (int) Character.MAX_VALUE);
 
-        // Mostrar el mayor número representable en short
+
         System.out.println("El mayor número representable en short es: " + Short.MAX_VALUE);
 
-        // Mostrar el mayor número representable en int
+
         System.out.println("El mayor número representable en int es: " + Integer.MAX_VALUE);
     }
 }
