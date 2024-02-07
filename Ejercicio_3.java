8de836d7fabcd40aa2fb94ae72785673fddbcf76
Josseluu <jslslpz@myuax.com>
2024-02-07 16:43:25 +0100

    .

--- Ejercicio_3.java
+++ Ejercicio_3.java
@@ -5,35 +5,35 @@ public class Ejercicio_3 {
         int begin = 1;
         int end = 100;
 
-        // Obtener todos los múltiplos de 5 entre begin y end
+
         int[] multiplosDe5 = obtenerMultiplosDe5(begin, end);
 
-        // Visualizar cada uno de los múltiplos de 5
+
         System.out.println("Múltiplos de 5 entre " + begin + " y " + end + ": ");
         for (int multiplo : multiplosDe5) {
             System.out.print(multiplo + " ");
         }
         System.out.println();
 
-        // Calcular la suma de los múltiplos de 5
+
         int suma = calcularSuma(multiplosDe5);
 
-        // Visualizar la suma de los múltiplos de 5
+
         System.out.println("La suma de los múltiplos de 5 es: " + suma);
     }
 
     public static int[] obtenerMultiplosDe5(int begin, int end) {
         int cantidadMultiplos = 0;
-        // Calcular la cantidad de múltiplos de 5 entre begin y end
+
         for (int i = begin; i <= end; i++) {
             if (i % 5 == 0) {
                 cantidadMultiplos++;
             }
         }
-        // Crear un array para almacenar los múltiplos de 5
+
         int[] multiplosDe5 = new int[cantidadMultiplos];
         int index = 0;
-        // Llenar el array con los múltiplos de 5
+
         for (int i = begin; i <= end; i++) {
             if (i % 5 == 0) {
                 multiplosDe5[index] = i;
@@ -45,7 +45,7 @@ public class Ejercicio_3 {
 
     public static int calcularSuma(int[] array) {
         int suma = 0;
-        // Sumar todos los valores del array
+
         for (int num : array) {
             suma += num;
         }
